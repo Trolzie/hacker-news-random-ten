aa64e91540cdbc669031c6d1b2465017b1c0235b
Trolzie <tlrc1984@gmail.com>
2022-12-13 11:36:02 +0100

    feat: added storyListItem with data + shuffled storyId array

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -1,7 +1,17 @@
+import StoryListItem from './storyListItem';
+
 async function getStories() {
   const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
   const data = await res.json();
   return data;
+}
+
+export function shuffleArray(array: string[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 export default async function StoriesPage() {
@@ -13,8 +23,8 @@ export default async function StoriesPage() {
       <h1>Stories</h1>
       <p>hi there</p>
       <ul>
-      {(stories).slice(0,10).map((id: string)=><li key={id}>
-        {id}
+        {shuffleArray(stories).slice(0,10).map((id: string)=><li key={id}>
+          <StoryListItem storyId={id}></StoryListItem>
         </li>)}
       </ul>
     </div>
