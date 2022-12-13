c5369381492bc251f9a0a867cb2e8b116ce8cfcb
Trolzie <tlrc1984@gmail.com>
2022-12-13 11:11:02 +0100

    feat: added stories route with 10 random story ids

---
+++ app/stories/page.tsx
@@ -0,0 +1,22 @@
+async function getStories() {
+  const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
+  const data = await res.json();
+  return data;
+}
+
+export default async function StoriesPage() {
+  const stories = await getStories();
+
+
+  return(
+    <div>
+      <h1>Stories</h1>
+      <p>hi there</p>
+      <ul>
+      {(stories).slice(0,10).map(id=><li key={id}>
+        {id}
+        </li>)}
+      </ul>
+    </div>
+  )
+}
