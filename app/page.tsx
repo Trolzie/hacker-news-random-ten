4ff83e25aaa5089ff66b5a83b3c86e63e29c642b
Trolzie <tlrc1984@gmail.com>
2022-12-13 21:53:17 +0100

    fix: removed unsued function breaking build because of multiple exports

--- app/page.tsx
+++ app/page.tsx
@@ -14,15 +14,6 @@ async function fetchStoriesId() {
 //   return data;
 // }
 
-export function getTenRandomStoryIds(array: string[]) {
-
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
 const randomizeArrayWithLength = (arr: string[], n: number) => {
   var result = new Array(n),
       len = arr.length,
